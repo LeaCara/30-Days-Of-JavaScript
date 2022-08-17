a3469d88d44f89691075fdf9b3c8d5dc9daccb7d
LeaCara <leandro.carancini@gmail.com>
2022-08-17 12:16:38 -0300

    Day 1: variable name and types

--- Exercise/introduction.js
+++ Exercise/introduction.js
@@ -4,7 +4,7 @@ console.log(typeof 5) // number
 console.log(typeof true) // boolean
 console.log(typeof null) // object type
 console.log(typeof undefined) // undefined
-let firstName
+let firstName = 'Asabeneh' // first name of a person
 console.log(firstName) // undefined, because it is not assigned to a value yet
 let emptyValue = null
 
@@ -18,3 +18,39 @@ let emptyValue = null
   let isMarried = true;
   This is a Multiple line comment
 */
+
+// valid variable names
+
+let capitalCity
+
+let first_name
+let last_name
+let is_married
+let capital_city
+
+let num1
+let num_1
+let _num_1
+let $num1
+let year2020
+let year_2020
+
+// Declaring different variables of different data types
+let age = 100 // age in years
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Declaring variables with number values
+
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+console.log(name, job, live)
