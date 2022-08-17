8705b0f0426aaeeebb1e6a944bf12ae95bb46be2
LeaCara <leandro.carancini@gmail.com>
2022-08-17 12:07:00 -0300

    Day 1: Commenting

--- Exercise/introduction.js
+++ Exercise/introduction.js
@@ -7,3 +7,14 @@ console.log(typeof undefined) // undefined
 let firstName
 console.log(firstName) // undefined, because it is not assigned to a value yet
 let emptyValue = null
+
+// commenting the code itself with a single comment
+// let firstName = 'Asabeneh'; single line comment
+// let lastName = 'Yetayeh'; single line comment
+
+/*
+  let location = 'Helsinki';
+  let age = 100;
+  let isMarried = true;
+  This is a Multiple line comment
+*/
