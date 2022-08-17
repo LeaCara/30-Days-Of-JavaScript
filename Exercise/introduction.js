abe4b566131424c0a8c886654739820f0425e03f
LeaCara <leandro.carancini@gmail.com>
2022-08-17 12:06:38 -0300

    Day 1: Variable types

--- Exercise/introduction.js
+++ Exercise/introduction.js
@@ -1,1 +1,9 @@
 console.log('Welcome to 30DaysOfJavaScript')
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+let firstName
+console.log(firstName) // undefined, because it is not assigned to a value yet
+let emptyValue = null
