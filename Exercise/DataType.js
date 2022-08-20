bcc79c398dab0e972137001b4f5fadfe8fda972e
lcarancini <leandro.carancini@gmail.com>
2022-08-19 22:39:44 -0300

    Day 3: operator, date and booleans comp

--- Exercise/DataType.js
+++ Exercise/DataType.js
@@ -426,3 +426,74 @@ let numInt = parseInt(num)
 console.log(numInt) // 9
 
 
+// Boolean
+// undefined and Null -> Falsy
+
+let job = undefined
+
+console.log(job) // undefined
+
+let numOne = 4
+let numTwo = 3
+let sum = numOne + numTwo
+let diff = numOne - numTwo
+let mult = numOne * numTwo
+let div = numOne / numTwo
+let remainder = numOne % numTwo
+let powerOf = numOne ** numTwo
+
+console.log(sum, diff, mult, div, remainder, powerOf) // 7,1,12,1.33,1, 64
+
+console.log(3 > 2)              // true, because 3 is greater than 2
+console.log(3 >= 2)             // true, because 3 is greater than 2
+console.log(3 < 2)              // false,  because 3 is greater than 2
+console.log(2 < 3)              // true, because 2 is less than 3
+console.log(2 <= 3)             // true, because 2 is less than 3
+console.log(3 == 2)             // false, because 3 is not equal to 2
+console.log(3 != 2)             // true, because 3 is not equal to 2
+console.log(3 == '3')           // true, compare only value
+console.log(3 === '3')          // false, compare both value and data type
+console.log(3 !== '3')          // true, compare both value and data type
+console.log(3 != 3)             // false, compare only value
+console.log(3 !== 3)            // false, compare both value and data type
+console.log(0 == false)         // true, equivalent
+console.log(0 === false)        // false, not exactly the same
+console.log(0 == '')            // true, equivalent
+console.log(0 == ' ')           // true, equivalent
+console.log(0 === '')           // false, not exactly the same
+console.log(1 == true)          // true, equivalent
+console.log(1 === true)         // false, not exactly the same
+console.log(undefined == null)  // true
+console.log(undefined === null) // false (equal in content and data type)  https://dorey.github.io/JavaScript-Equality-Table/
+console.log(NaN == NaN)         // false, not equal
+console.log(NaN === NaN)        // false
+console.log(typeof NaN)         // number
+
+console.log('mango'.length == 'avocado'.length)  // false
+console.log('mango'.length != 'avocado'.length)  // true
+console.log('mango'.length < 'avocado'.length)   // true
+console.log('milk'.length == 'meat'.length)      // true
+console.log('milk'.length != 'meat'.length)      // false
+console.log('tomato'.length == 'potato'.length)  // true
+console.log('python'.length > 'dragon'.length)   // false
+
+// ? ternary operator
+
+alert('Welcome to 30DaysOfJavaScript')
+
+prompt('required text', 'optional text')
+
+const agree = confirm('Are you sure you like to delete? ')
+console.log(agree) // result will be true or false based on what you click on the dialog box
+
+const now = new Date()
+console.log(now) // Sat Jan 04 2020 00:56:41 GMT+0200 (Eastern European Standard Time)
+
+const now = new Date()
+console.log(now.getFullYear()) // 2020
+
+const allSeconds = Date.now() //
+console.log(allSeconds) // 1578092201341, this is the number of seconds passed from January 1, 1970 to January 4, 2020 00:56:41
+
+const timeInSeconds = new Date().getTime()
+console.log(allSeconds == timeInSeconds) // true
